361818a4b55c32240bf618e9497c1d81ce503e7a
Administrator <Administrator@10PC331>
2024-03-22 16:15:10 +0700

    Add project files.

---
+++ QLBH/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QLBH.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public required string Name { get; set; }
+        public List<Product> Products { get; set; }
+
+
+    }
+}
